f75cfd8973dc71d6051c89d3bd1e12dcfc93c689
IFraimG <kulsasha55@gmail.com>
2024-03-26 18:40:20 +0300

    update configuration

--- app/src/main/java/com/example/finalprodproject/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/finalprodproject/ui/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.finalprodproject;
+package com.example.finalprodproject.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.finalprodproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
