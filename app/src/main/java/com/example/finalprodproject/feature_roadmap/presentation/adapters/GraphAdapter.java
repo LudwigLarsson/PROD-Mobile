b998b6bb78d638db64aefb6dd629c6123edf027a
IFraimG <kulsasha55@gmail.com>
2024-04-01 05:09:57 +0300

    scroll bar finish

--- app/src/main/java/com/example/finalprodproject/feature_roadmap/presentation/adapters/GraphAdapter.java
+++ app/src/main/java/com/example/finalprodproject/feature_roadmap/presentation/adapters/GraphAdapter.java
@@ -1,5 +1,6 @@
 package com.example.finalprodproject.feature_roadmap.presentation.adapters;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,16 +9,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.finalprodproject.R;
 import com.example.finalprodproject.databinding.CircleItemBinding;
 import com.example.finalprodproject.feature_main.data.RoadmapItem;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GraphAdapter extends RecyclerView.Adapter<GraphAdapter.ViewHolder> {
     private final List<RoadmapItem> items;
+    private final List<Integer> drawablesList = new ArrayList<>();
 
     public GraphAdapter(List<RoadmapItem> items) {
         this.items = items;
+
+        drawablesList.add(R.drawable.color_border_1);
+        drawablesList.add(R.drawable.color_border_2);
+        drawablesList.add(R.drawable.color_border_3);
+        drawablesList.add(R.drawable.color_border_4);
     }
 
     @NonNull
@@ -31,24 +41,28 @@ public class GraphAdapter extends RecyclerView.Adapter<GraphAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull GraphAdapter.ViewHolder holder, int position) {
 //        holder.setIsRecyclable(false);
         holder.hideElements();
+        int indexRandom = new Random().nextInt(drawablesList.size());
         if (position % 2 == 0) {
                 holder.binding.circleTitleFirst.setVisibility(View.VISIBLE);
                 holder.binding.circleImageFirst.setVisibility(View.VISIBLE);
-                if (position + 1 != items.size())  holder.binding.circleRightFirst.setVisibility(View.VISIBLE);
+                if (position + 1 != items.size()) holder.binding.circleRightFirst.setVisibility(View.VISIBLE);
                 holder.binding.circleRightPagerFirst.setVisibility(View.VISIBLE);
 
                 holder.binding.circleTitleFirst.setText(items.get(position).getText());
                 holder.binding.circleRightPagerFirst.setText(Integer.toString(items.get(position).getScores()));
+
+                holder.binding.circleImageFirst.setBackgroundResource(drawablesList.get(indexRandom));
                 Glide.with(holder.binding.getRoot()).load(items.get(position).getImage()).into(holder.binding.circleImageFirst);
         } else {
                 holder.binding.circleTitleSecond.setVisibility(View.VISIBLE);
                 holder.binding.circleImageSecond.setVisibility(View.VISIBLE);
-                if (position + 1 != items.size())
-                    holder.binding.circleLeftSecond.setVisibility(View.VISIBLE);
+                if (position + 1 != items.size()) holder.binding.circleLeftSecond.setVisibility(View.VISIBLE);
                 holder.binding.circleRightPagerSecond.setVisibility(View.VISIBLE);
 
                 holder.binding.circleTitleSecond.setText(items.get(position).getText());
                 holder.binding.circleRightPagerSecond.setText(Integer.toString(items.get(position).getScores()));
+
+                holder.binding.circleImageSecond.setBackgroundResource(drawablesList.get(indexRandom));
                 Glide.with(holder.binding.getRoot()).load(items.get(position).getImage()).into(holder.binding.circleImageSecond);
         }
     }
