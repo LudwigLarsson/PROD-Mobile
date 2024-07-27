b361ce4bf4ea6e525dd7913795e48082ad859d9d
Maria Vasina <mariavasina@inbox.ru>
2024-07-27 22:04:55 +0300

    Api path changed

--- app/src/main/java/com/example/finalprodproject/utils/Constants.java
+++ app/src/main/java/com/example/finalprodproject/utils/Constants.java
@@ -1,5 +1,5 @@
 package com.example.finalprodproject.utils;
 
 public class Constants {
-    public static final String USER_API_PATH = "http://82.97.241.151:8081/api/";
+    public static final String USER_API_PATH = "http://185.119.58.210:8081/api/";
 }
